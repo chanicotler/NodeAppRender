033f0ec521f9bf9379a163f522a8a28ac593e4c1
chani <chanicotler@gmail.com>
2024-01-24 17:10:01 +0200

    Update

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
-const sdk = require('api')('@render-api/v1.0#jw0lr5hblce');
+const sdk = require('api')('@render-api/v1.0#jw0325lr5hblce');
 
-sdk.auth('rnd_CcweNi8LrbNAw0uBT0MfcaRd8');
+sdk.auth('rnd_CcweNi8LrbNAw0uBT0MSNHfcaRd8');
 sdk.getServices({ limit: '20' })
     .then(({ data }) => console.log(data))
     .catch(err => console.error(err));

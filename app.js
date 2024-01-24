4cba1c5d4ad4e2d10c1ee0130845872ad45060c5
chani <chanicotler@gmail.com>
2024-01-24 15:40:11 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+const sdk = require('api')('@render-api/v1.0#jw0325lr5hblce');
+
+sdk.auth('rnd_CcweNi8LrbNAw0uBT0MSNHfcaRd8');
+sdk.getServices({ limit: '20' })
+    .then(({ data }) => console.log(data))
+    .catch(err => console.error(err));
